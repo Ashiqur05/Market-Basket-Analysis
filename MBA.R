d08df89784f00907994b26427a477efe9eb78ae8
Ashiqur05 <rahmanashiqur41@yahoo.com>
2019-10-30 13:49:46 +0100

    data quality

--- MBA.R
+++ MBA.R
@@ -64,8 +64,186 @@ on.exit(dbDisconnect(mydb))
 
 
 #*************************************************************************************************************
+library(dplyr)
 
 
+#Check if Data Frame is NULL
+sum(is.null(line_item))
+sum(is.null(products))
+sum(is.null(orders))
 
 
+#There are 218 missing values in products data frame. We will remove these rows in next step and check again
+
+#products <- products[complete.cases(products), ]
+#sum(is.na(products))
+
+#Show head and summary of line_item
+head(line_item)
+summary(line_item)
+str(line_item)
+glimpse(line_item)
+dim(line_item)
+
+
+#Show head and summary of orders
+head(orders)
+summary(orders)
+str(orders)
+glimpse(orders)
+dim(orders)
+
+#Show head and summary of products
+head(products)
+summary(products)
+str(products)
+glimpse(products)
+dim(products)
+colnames(products)
+
+# Check that all orders in line_item are present in our orders dataset. 
+# Exclude from line_item any rows that do not meet that condition.
+
+#using inner_join
+new_Line_item<-inner_join(line_item,orders,by="id_order")
+dim(new_Line_item)
+head(new_Line_item)
+
+#list of the value of id_order which is present in order dataset
+line_item[line_item$id_order %in% new_Line_item$id_order,]
+count(line_item[line_item$id_order %in% new_Line_item$id_order,])
+
+#list of the value of id_order which is not present  in order table
+count(line_item[!(line_item$id_order %in% new_Line_item$id_order),])
+
+#############################################################################################################
+testdf <- data.frame(
+        colA = c("A", "A", "B", "C", "D", "E"),
+        value = c(1,2,3,4,5,6)
+)
+testdf$colA <- as.character(testdf$colA)
+
+testdf2 <- data.frame(
+        colA = c("A", "B", "C", "F", "X"),
+        value2 = c(1,2,3,4,5)
+)
+testdf2$colA <- as.character(testdf2$colA)
+
+
+jointest <- inner_join(testdf, testdf2, by = "colA")
+
+testdf[testdf$colA %in% testdf2$colA]
+testdf[testdf$colA %in% testdf2$colA,]
+unique(testdf[testdf$colA %in% testdf2$colA,])
+
+a = c(1,2,3,4,5)
+b = c(3,4,5,6,7)
+
+
+a %in% b
+
+testdf["colA"]
+
+
+#using merge
+new_Line_item <- merge(orders, line_item, by="id_order")
+dim(new_Line_item)
+head(new_Line_item)
+
+#val = c()
+#for (item in orders$id_order){ if (item %in% line_item$id_order)   {val = append(val, item)}}
+##############################################################################################################
+
+
+#t=distinct(select(line_item,id_order))
+#r=distinct(select(orders,id_order))
+
+
+#Exclude from line_item any rows from orders that are not “Completed”.
+#Filter only completed orders
+
+#list of the panding item in line_item table
+new_Line_item_pan <- filter(new_Line_item, state == "Pending")
+dim(new_Line_item_pan)
+head(new_Line_item_pan)
+
+#list of the Cancelled item in line_item table
+new_Line_item_can <- filter(new_Line_item, state == "Cancelled")
+dim(new_Line_item_can)
+head(new_Line_item_can)
+
+#list of the Completed item in line_item table
+new_Line_item_com <- filter(new_Line_item, state == "Completed")
+dim(new_Line_item_com)
+head(new_Line_item_com)
+
+##############dim(setdiff(line_item,orders))
+
+##########y=dim(line_item)-dim(orders)
+# Check that all products in line_item are present in the products dataset. 
+# Exclude from line_item any rows that do not meet that condition.
+
+#new_Line_item2 <- inner_join(line_item, products, by="sku")
+#dim(new_Line_item2)
+#head(new_Line_item2)
+
+new_Line_item2 <- inner_join(new_Line_item_com, products, by="sku")
+dim(new_Line_item2)
+head(new_Line_item2)
+
+#list of the value of sku which is present in order dataset
+line_item[line_item$sku %in% new_Line_item2$sku,]
+count(line_item[line_item$sku %in% new_Line_item2$sku,])
+
+#list of the value of sku which is not present  in order table
+count(line_item[!(line_item$sku %in% new_Line_item2$sku),])
+
+dim(new_Line_item_com)
+colnames(new_Line_item_com)
+dim(new_Line_item2)
+colnames(new_Line_item2)
+head(new_Line_item2)
+
+#Explore the relationship between prices in line_item and order:
+
+
+#??what is the price in product table
+
+new_Line_item2_price<-select(new_Line_item2,id_order,unit_price,product_quantity,total_paid)
+head(new_Line_item2_price)
+
+new_Line_item2_price %>% group_by(id_order) %>% summarise(idcount=count(product_quantity))
+#final <- inner_join(new_Line_item_com, new_Line_item2)
+#dim(final)
+#glimpse(final)
+#head(final)
+
+# Remove unnecessary columns
+
+final$id <- NULL
+final$date <- NULL
+final$created_date <- NULL
+final$state <- NULL
+final$salable <- NULL
+final$stock_qty <- NULL
+final$purchasable <- NULL
+
+dim(final)
+head(final)
+# Remove outliers from final
+
+final <- filter(final, unit_price >= 0  & product_quantity < 999)
+
+dim(final)
+
+final %>% 
+        id_order <- as.factor(id_order) %>% 
+        final <- group_by(id_order) %>% mutate(id = row_number())
+head(final)
+dim(final)
+
+# Explore the relationship between prices in line_item and order:
+# Do the prices of the orders and the sum of the prices of each item in the order match? 
+# If not, can you make some assumptions that explain the differences?
+#Exclude from line_items the rows with differences that you cannot explain.
 
